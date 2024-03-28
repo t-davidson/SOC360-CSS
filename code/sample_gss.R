d0313364157d906286c2b0623849d7dfe98e7413
t-davidson <thomas.davidson@rutgers.edu>
2024-03-27 21:59:38 -0400

    Modified GSS code and data

--- code/sample_gss.R
+++ code/sample_gss.R
@@ -1,13 +1,31 @@
 library(tidyverse)
 library(haven)
 
-data <- read_dta("data/GSS2018.dta")
+data <- read_dta("../data/GSS2018.dta")
 
-data <- data %>% select(age, sex, race, realrinc, educ, hrs1, marital, bible, childs,  paeduc, maeduc, degree) %>%
+data_final <- data %>% select(age, sex, race, sibs,
+                                    paeduc, maeduc,
+                                    family16, fund16, incom16, relig16, mawrkgrw,
+                                    othlang, born, parborn, granborn,
+                                    zodiac,
+                                    degree ) %>%
     drop_na(degree) %>%
-    filter(age <= 89) %>% haven::zap_labels() %>%
+    filter(age >= 21 & age <= 89) %>%
+    haven::zap_labels() %>%
     mutate(sex = ifelse(sex == 1, 1, 0),
-           race = as.factor(race),
            degree = ifelse(degree >= 3, 1, 0))
-write_csv(data, "data/gss_sample.csv")
+
+# Define a function to calculate the mode that works for both numeric and categorical data
+get_mode <- function(x) {
+    ux <- unique(na.omit(x))
+    mode <- ux[which.max(tabulate(match(x, ux)))]
+    return(as.character(mode))
+}
+
+# Fill NA values with the mode for each column, works for all types of columns
+data_filled <- data_final %>%
+    mutate(across(everything(), ~ifelse(is.na(.), get_mode(.), .)))
+
+write_csv(data_filled, "../data/2018_gss_sample.csv")
+
 
