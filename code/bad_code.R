49fb44149f7f92b78a7ab015ebe915d52a6b186b
t-davidson <thomas.davidson@rutgers.edu>
2025-09-03 22:22:01 -0400

    Adding code example

---
+++ code/bad_code.R
@@ -0,0 +1,11 @@
+set.seed(8901)
+# Fix the code below
+
+calculate_mean <- function() {
+    mean <- sum(x) / len(x)
+    return()
+}}
+
+# This line should run once the code works. What is your answer?
+round(calculate_mean(runif(1E6)),2)
+
